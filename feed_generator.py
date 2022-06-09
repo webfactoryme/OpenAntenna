eb7c359a4c1db30ddf30ff007eb2e115c2a9952b
WebFactory.me <106634182+webfactoryme@users.noreply.github.com>
2022-06-09 11:02:26 -0500

    Update feed_generator.py

--- feed_generator.py
+++ feed_generator.py
@@ -27,17 +27,17 @@ def make_feed():
     p = Podcast()
     p.set_generator("Open Antenna Podcast Generator", (1,0,0))
     p.name = show_data[1]
-    p.authors.append(Person('Lead and Circus (LeadandCircus.com)', 'contact@leadandcircus.com'))
-    p.website = 'https://leadandcircus.com'
-    p.copyright = 'Lead and Circus 2021'
+    p.authors.append(Person('Open Antenna (OpenAntenna.org)', 'contact@openantenna.org'))
+    p.website = 'https://openantenna.org'
+    p.copyright = 'Open Antenna 2022'
     p.description = show_data[2]
     p.language = 'en'
-    p.feed_url = 'https://leadandcircus.com/static/feed/podcast.xml'
+    p.feed_url = 'https://openantenna.org/static/feed/podcast.xml'
     p.category = Category('News','Politics')
     p.image = show_data[3]
     p.explicit = False
     p.complete = False
-    p.owner = Person('Lead and Circus', 'contact@leadandcircus.com')
+    p.owner = Person('Open Antenna', 'contact@openantenna.org')
     
     # Add episodes
     for item in episode_data:
@@ -45,7 +45,7 @@ def make_feed():
         e1.id = item[4]
         e1.title = item[1]
         e1.summary = htmlencode(item[2])
-        e1.link = 'http://leadandcircus.com/episode/sdasd'
+        e1.link = 'http://openantenna.org/episode/sdasd'
         hour_duration = int(item[6].split(':')[0])
         minute_duration = int(item[6].split(':')[1])
         second_duration =  int(item[6].split(':')[2])
