0048ac92ca52f69a0bc3715ace0ea22eae21c155
Jimmy Ryan <contact@jimmyryan.info>
2022-06-18 16:12:22 -0500

    Update to specify the openantenna database

--- tools/openantenna_installer.sh
+++ tools/openantenna_installer.sh
@@ -23,7 +23,7 @@ timeout 2 python3 /var/www/OpenAntenna/__init__.py &&
 
 # Create User in OpenAntenna
 mysql openantenna << EOF 
-INSERT INTO users (id,name,picture,email,phone,password,date_registered,last_login,user_type,status) VALUES (NULL, 'User', 'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png', 'emailme@email.com', '9999999999', 'password', '2022-06-18 13:50:39', '2022-06-18 13:50:39', 'admin', 'active'); 
+INSERT INTO openantenna.users (id,name,picture,email,phone,password,date_registered,last_login,user_type,status) VALUES (NULL, 'User', 'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png', 'emailme@email.com', '9999999999', 'password', '2022-06-18 13:50:39', '2022-06-18 13:50:39', 'admin', 'active'); 
 EOF
 
 echo "You can now run 'python3 /var/www/OpenAntenna/__init__.py' to start the server"
