2df5c912b6243913da71e180271b9e487fe6f2d8
jimmy <contact@webfactory.me>
2022-06-18 20:56:20 -0500

    yet even more updates

--- tools/openantenna_installer.sh
+++ tools/openantenna_installer.sh
@@ -9,8 +9,19 @@ sudo pip3 install flask flask_sqlalchemy mysqlclient &&
 
 cd /var/www/ &&
 
-rm -rf OpenAntenna/ ||: &&
-# Clone OpenAntenna into /var/www/ folder
+rm -rf openantenna/ ||: &&
+
+mkdir openantenna &&
+
+cd /var/www/openantenna/ &&
+
+wget https://raw.githubusercontent.com/webfactoryme/OpenAntenna/main/configs/openantenna.conf &&
+
+cd /etc/apache2/sites-available/ &&
+
+wget https://github.com/webfactoryme/OpenAntenna/blob/main/configs/openantenna.conf &&
+
+# Clone OpenAntenna into /var/www/openantenna/ folder
 git clone https://github.com/webfactoryme/OpenAntenna.git &&
 
 # Add configurations for Apache 
@@ -19,11 +30,14 @@ git clone https://github.com/webfactoryme/OpenAntenna.git &&
 mysql -e "CREATE DATABASE IF NOT EXISTS openantenna;" || true &&
 
 # Run OpenAntenna Server for first time to generate DB tables
-timeout 2 python3 /var/www/OpenAntenna/__init__.py &&
+timeout 2 python3 /var/www/openantenna/OpenAntenna/__init__.py &&
+
+# Run OpenAntenna with Apache
+sudo a2ensite openantenna.conf &&
 
 # Create User in OpenAntenna
 mysql openantenna << EOF 
 INSERT INTO openantenna.users (id,name,picture,email,phone,password,date_registered,last_login,user_type,status) VALUES (NULL, 'User', 'h>
 EOF
 
-echo "You can now run 'python3 /var/www/OpenAntenna/__init__.py' to start the server"
+echo "You can now run 'python3 /var/www/openantenna/OpenAntenna/__init__.py' to start the server"
